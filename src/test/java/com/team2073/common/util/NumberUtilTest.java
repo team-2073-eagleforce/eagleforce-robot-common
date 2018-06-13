b83aaf3ca78ec631c887952ca9a2933e73748224
TheMinja <retrominh@gmail.com>
2018-06-12 23:42:04 -0700

    added unit tests

---
+++ src/test/java/com/team2073/common/util/NumberUtilTest.java
@@ -0,0 +1,19 @@
+package com.team2073.common.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+class NumberUtilTest {
+
+    @Test
+    void trim() {
+        Assertions.assertTrue(false);
+    }
+
+    @Test
+    void trime() {
+        Assertions.assertTrue(true);
+    }
+}
---
+++ src/test/java/com/team2073/common/util/Something.java
@@ -0,0 +1,11 @@
+package com.team2073.common.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Something {
+    @Test
+    void trimee() {
+        Assertions.assertTrue(true);
+    }
+}
