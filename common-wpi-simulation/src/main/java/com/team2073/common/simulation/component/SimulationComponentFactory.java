ec0b71756da008db0ff07305ca2ae19ba3219421
Jason2073 <stanleytechrepair@gmail.com>
2018-09-04 00:12:43 -0700

    COMMON-164: Created Method in SimulationComponentFactory for creating DigitalInputs. Tests will be included with the SimulationElevator in COMMON-165.

--- common-wpi-simulation/src/main/java/com/team2073/common/simulation/component/SimulationComponentFactory.java
+++ common-wpi-simulation/src/main/java/com/team2073/common/simulation/component/SimulationComponentFactory.java
@@ -1,6 +1,7 @@
 package com.team2073.common.simulation.component;
 
 import com.team2073.common.simulation.model.SimulationMechanism;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 import static org.mockito.Mockito.*;
 
@@ -21,5 +22,19 @@ public class SimulationComponentFactory {
 		return solenoid;
 	}
 
+	/**
+	 * @param mechanism
+	 * @param location    The center position of the accompanying mechanism that the sensor will read true at.
+	 * @param sensorWidth How much to look above and below the sensor to read high. Ex. location = 5 and sensorWidth = 2 would read high from 3 to 7.
+	 * @return
+	 */
+	public static DigitalInput createSimulationDigitalInput(SimulationMechanism mechanism, double location, double sensorWidth) {
+
+		DigitalInput digitalInput = mock(DigitalInput.class);
+
+		when(digitalInput.get()).thenReturn((mechanism.position() > location - sensorWidth) && (mechanism.position() < location + sensorWidth));
+
+		return digitalInput;
+	}
 
 }
