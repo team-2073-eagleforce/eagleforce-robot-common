b105270de3ec64e8d901e85fc7d78fe21e477267
Jason2073 <stanleytechrepair@gmail.com>
2018-11-24 13:48:07 -0800

    COMMON-201: Updating talonUtil so that getSelectedSensorPosition() is updated at a reasonable rate for our robots when talons are configured for sensor mode.

--- common-wpi-util/src/main/java/com/team2073/common/util/TalonUtil.java
+++ common-wpi-util/src/main/java/com/team2073/common/util/TalonUtil.java
@@ -24,9 +24,10 @@ public class TalonUtil {
 		public int MOTION_CONTROL_FRAME_PERIOD_MS = 100;
 		public int GENERAL_STATUS_FRAME_RATE_MS = 5;
 		public int FEEDBACK_STATUS_FRAME_RATE_MS = 100;
-		public int QUAD_ENCODER_STATUS_FRAME_RATE_MS = 10; /* default is 100ms, using 10 to match robot loops */
+		public int QUAD_ENCODER_STATUS_FRAME_RATE_MS = 100;
 		public int ANALOG_TEMP_VBAT_STATUS_FRAME_RATE_MS = 100;
 		public int PULSE_WIDTH_STATUS_FRAME_RATE_MS = 100;
+		public int BASE_PIDF_STATUS_FRAME_RATE_MS = 100;
 
 		public VelocityMeasPeriod VELOCITY_MEASUREMENT_PERIOD = VelocityMeasPeriod.Period_100Ms;
 		public int VELOCITY_MEASUREMENT_ROLLING_AVERAGE_WINDOW = 64;
@@ -37,6 +38,13 @@ public class TalonUtil {
 
 	private static final Configuration kDefaultConfiguration = new Configuration();
 	private static final Configuration kSlaveConfiguration = new Configuration();
+	private static final Configuration kSensorConfiguration = new Configuration();
+
+	public enum ConfigurationType{
+		DEFAULT,
+		SLAVE,
+		SENSOR
+	}
 
 	static {
 		// This control frame value seems to need to be something reasonable to avoid the Talon's
@@ -48,6 +56,11 @@ public class TalonUtil {
 		kSlaveConfiguration.QUAD_ENCODER_STATUS_FRAME_RATE_MS = 1000;
 		kSlaveConfiguration.ANALOG_TEMP_VBAT_STATUS_FRAME_RATE_MS = 1000;
 		kSlaveConfiguration.PULSE_WIDTH_STATUS_FRAME_RATE_MS = 1000;
+
+
+
+		kSensorConfiguration.QUAD_ENCODER_STATUS_FRAME_RATE_MS = 10;
+		kSensorConfiguration.FEEDBACK_STATUS_FRAME_RATE_MS = 10;
 	}
 
 	/**
@@ -55,8 +68,21 @@ public class TalonUtil {
 	 * @param talon
 	 * @return the talon
 	 */
-	public static IMotorControllerEnhanced resetTalon(IMotorControllerEnhanced talon) {
-		Configuration config = kDefaultConfiguration;
+	public static IMotorControllerEnhanced resetTalon(IMotorControllerEnhanced talon, ConfigurationType configType) {
+		Configuration config;
+		switch (configType) {
+			case SENSOR:
+				config = kSensorConfiguration;
+				break;
+			case SLAVE:
+				config = kSlaveConfiguration;
+				break;
+			case DEFAULT:
+				config = kDefaultConfiguration;
+				break;
+			default:
+				config = kDefaultConfiguration;
+		}
 		talon.set(ControlMode.PercentOutput, 0.0);
 
 		talon.changeMotionControlFramePeriod(config.MOTION_CONTROL_FRAME_PERIOD_MS);
@@ -122,9 +148,84 @@ public class TalonUtil {
 				config.ANALOG_TEMP_VBAT_STATUS_FRAME_RATE_MS, kTimeoutMs);
 		talon.setStatusFramePeriod(StatusFrameEnhanced.Status_8_PulseWidth,
 				config.PULSE_WIDTH_STATUS_FRAME_RATE_MS, kTimeoutMs);
+		talon.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, config.BASE_PIDF_STATUS_FRAME_RATE_MS, kTimeoutMs);
 
 		talon.setControlFramePeriod(ControlFrame.Control_3_General, config.CONTROL_FRAME_PERIOD_MS);
 
 		return talon;
+	}
+
+	public static IMotorController resetVictor(IMotorController victor, ConfigurationType configType) {
+		Configuration config;
+		switch (configType) {
+			case SENSOR:
+				config = kSensorConfiguration;
+				break;
+			case SLAVE:
+				config = kSlaveConfiguration;
+				break;
+			case DEFAULT:
+				config = kDefaultConfiguration;
+				break;
+			default:
+				config = kDefaultConfiguration;
+		}
+		victor.set(ControlMode.PercentOutput, 0.0);
+
+		victor.changeMotionControlFramePeriod(config.MOTION_CONTROL_FRAME_PERIOD_MS);
+		victor.clearMotionProfileHasUnderrun(kTimeoutMs);
+		victor.clearMotionProfileTrajectories();
+
+		victor.clearStickyFaults(kTimeoutMs);
+
+		victor.overrideLimitSwitchesEnable(config.ENABLE_LIMIT_SWITCH);
+
+		// Turn off re-zeroing by default.
+		victor.configSetParameter(
+				ParamEnum.eClearPositionOnLimitF, 0, 0, 0, kTimeoutMs);
+		victor.configSetParameter(
+				ParamEnum.eClearPositionOnLimitR, 0, 0, 0, kTimeoutMs);
+
+		victor.configNominalOutputForward(0, kTimeoutMs);
+		victor.configNominalOutputReverse(0, kTimeoutMs);
+		victor.configNeutralDeadband(config.NEUTRAL_DEADBAND, kTimeoutMs);
+
+		victor.configPeakOutputForward(1.0, kTimeoutMs);
+		victor.configPeakOutputReverse(-1.0, kTimeoutMs);
+
+		victor.setNeutralMode(config.NEUTRAL_MODE);
+
+		victor.configForwardSoftLimitThreshold(config.FORWARD_SOFT_LIMIT, kTimeoutMs);
+		victor.configForwardSoftLimitEnable(config.ENABLE_SOFT_LIMIT, kTimeoutMs);
+
+		victor.configReverseSoftLimitThreshold(config.REVERSE_SOFT_LIMIT, kTimeoutMs);
+		victor.configReverseSoftLimitEnable(config.ENABLE_SOFT_LIMIT, kTimeoutMs);
+		victor.overrideSoftLimitsEnable(config.ENABLE_SOFT_LIMIT);
+
+		victor.setInverted(config.INVERTED);
+		victor.setSensorPhase(config.SENSOR_PHASE);
+
+		victor.selectProfileSlot(0, 0);
+
+
+		victor.configOpenloopRamp(config.OPEN_LOOP_RAMP_RATE, kTimeoutMs);
+		victor.configClosedloopRamp(config.CLOSED_LOOP_RAMP_RATE, kTimeoutMs);
+
+		victor.configVoltageCompSaturation(0.0, kTimeoutMs);
+		victor.configVoltageMeasurementFilter(32, kTimeoutMs);
+		victor.enableVoltageCompensation(false);
+
+		victor.setStatusFramePeriod(StatusFrame.Status_1_General,
+				config.GENERAL_STATUS_FRAME_RATE_MS, kTimeoutMs);
+		victor.setStatusFramePeriod(StatusFrame.Status_2_Feedback0,
+				config.FEEDBACK_STATUS_FRAME_RATE_MS, kTimeoutMs);
+
+		victor.setStatusFramePeriod(StatusFrame.Status_4_AinTempVbat,
+				config.ANALOG_TEMP_VBAT_STATUS_FRAME_RATE_MS, kTimeoutMs);
+		victor.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, config.BASE_PIDF_STATUS_FRAME_RATE_MS, kTimeoutMs);
+
+		victor.setControlFramePeriod(ControlFrame.Control_3_General, config.CONTROL_FRAME_PERIOD_MS);
+
+		return victor;
 	}
 }
