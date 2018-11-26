a0a0cf4522bff792fdcd46c00997287fa5c30597
preston <preston@42lines.net>
2018-11-26 17:51:42 -0600

    HOTFIX: Adding file to test dir so the iml files behave properly

---
+++ common-wpi-util-test/src/test/java/com/team2073/common/wiptest/GitKeep.java
@@ -0,0 +1,11 @@
+package com.team2073.common.wiptest;
+
+/**
+ * @author Preston Briggs
+ */
+public class GitKeep {
+
+    // Only exists so this directory is checked into version control
+    // Once this module has any other real class in this directory, delete this class
+
+}
