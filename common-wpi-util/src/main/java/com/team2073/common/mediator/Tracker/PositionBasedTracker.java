5ae5daa7b305b8914a15b60dc9d60a03d30bb442
TheMinja <retrominh@gmail.com>
2019-01-19 12:03:12 -0800

    COMMON-213: Finished Generifying Conflict, Request, and Tracker

---
+++ common-wpi-util/src/main/java/com/team2073/common/mediator/Tracker/PositionBasedTracker.java
@@ -0,0 +1,5 @@
+package com.team2073.common.mediator.Tracker;
+
+public interface PositionBasedTracker extends Tracker<Double>{
+
+}
--- common-wpi-util/src/main/java/com/team2073/common/mediator/request/Request.kt
+++ common-wpi-util/src/main/java/com/team2073/common/mediator/request/Request.kt
@@ -3,7 +3,7 @@ package com.team2073.common.mediator.request
 import com.team2073.common.mediator.condition.Condition
 import com.team2073.common.mediator.subsys.ColleagueSubsystem
 
-data class Request<T>(val subsystem: Class<ColleagueSubsystem<T>>,
+data class Request<T>(val subsystem: Class<out ColleagueSubsystem<T>>,
                       val condition: Condition<T>) {
 
     var hasBeenRequested = false
