24c3ac897834588f68c3b2c0e173edfbc8991f65
Andy Wang <andy91441@gmail.com>
2022-10-29 13:58:19 -0700

    Added Alert and Tunable class

---
+++ common-util/src/main/java/com/team2073/common/util/PrintingUtil.java
@@ -0,0 +1,20 @@
+package com.team2073.common.util;
+
+public class PrintingUtil {
+
+    public PrintingUtil() {
+    }
+
+    //... means multi-arg meaning you can pass
+    public void addPrintMessage(String name, boolean print) {
+
+    }
+
+    public void addPrintMessage(Double...print) {
+
+    }
+
+    public void addPrintMessage(String...print) {
+
+    }
+}
