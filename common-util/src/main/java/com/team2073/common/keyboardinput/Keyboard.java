8f5dae1cbf21c1cf2666c3ddcd798c39a8b3808b
Lucian Mayers-Viseroi <lgmv7409@gmail.com>
2023-01-30 16:41:53 -0800

    added numbers to available keys

--- common-util/src/main/java/com/team2073/common/keyboardinput/Keyboard.java
+++ common-util/src/main/java/com/team2073/common/keyboardinput/Keyboard.java
@@ -21,7 +21,7 @@ public class Keyboard  {
 
     private HashMap<String, NetworkTableEntry> keys = new HashMap<>();
 
-    public String[] availableKeys = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N" , "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "<", ">"};
+    public String[] availableKeys = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N" , "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ",", ".","0","1","2","3","4","5","6","7","8","9"};
 
     private ArrayList<String> allKeys = new ArrayList<>();
     NetworkTableInstance ntinstance = NetworkTableInstance.getDefault();
